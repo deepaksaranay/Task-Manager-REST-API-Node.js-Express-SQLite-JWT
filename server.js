537cf12dbeebce04ea3e625bce9320a25037e4f5
DEEPAK KUMAR <139011150+deepaksaranay@users.noreply.github.com>
2026-07-13 13:14:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+
+const authRoutes = require("./authRoutes");
+const taskRoutes = require("./taskRoutes");
+
+// Database Connection
+require("./db");
+
+const app = express();
+
+// Middleware
 app.use(express.json());
-app.get("/",(req,res)=>res.json({message:"Task Manager API"}));
-app.listen(3000,()=>console.log("Server running on http://localhost:3000"));
+
+// Default Route
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Task Manager REST API is Running"
+    });
+});
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+// 404 Route
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: "Route Not Found"
+    });
+});
+
+// Server
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
