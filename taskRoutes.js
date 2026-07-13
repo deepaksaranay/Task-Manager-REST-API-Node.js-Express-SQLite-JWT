6b47b44b1ae10323a0a70fec908a38617906f2fa
DEEPAK KUMAR <139011150+deepaksaranay@users.noreply.github.com>
2026-07-13 13:27:14 +0530

    Update taskRoutes.js

--- taskRoutes.js
+++ taskRoutes.js
@@ -1,1 +1,30 @@
 module.exports={};
+const express = require("express");
+const router = express.Router();
+
+const auth = require("../middleware/auth");
+
+const {
+    createTask,
+    getTasks,
+    getTaskById,
+    updateTask,
+    deleteTask
+} = require("../controllers/taskController");
+
+// Create Task
+router.post("/", auth, createTask);
+
+// Get All Tasks
+router.get("/", auth, getTasks);
+
+// Get Task By ID
+router.get("/:id", auth, getTaskById);
+
+// Update Task
+router.put("/:id", auth, updateTask);
+
+// Delete Task
+router.delete("/:id", auth, deleteTask);
+
+module.exports = router;
